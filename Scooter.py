4b84da2959d784dbabd4f85473454678b1c84c1a
Robowa <marceli3@outlook.es>
2023-10-01 21:51:17 -0300

    Cambios varios
    Se hicieron cambios varios

--- Scooter.py
+++ Scooter.py
@@ -3,18 +3,26 @@ from Tecnologia import Tecnologia
 
 class Scooter(Transporte, Tecnologia):
     def __init__(self, voltaje: int, precio: float, eficiencia: str, marca: str, aro: float, velocidad: int, peso: float, costoDespachoBase: int = 4000):
-        Transporte().__init__(self, costoDespachoBase)
-        Tecnologia().__init__(self, voltaje, precio, eficiencia, marca)
+        super().__init__(costoDespachoBase)
+        Tecnologia.__init__(self, voltaje, precio, eficiencia, marca)
         self.__aro = aro
         self.__velocidad = velocidad
         self.__peso = peso
         
-    def calcularDescuento(self):
-        return Transporte.calcularDespacho()
-    
-    def __str__(self) -> str:
-        return f"{Tecnologia().__str__()}\nAro: {self.__aro}\nVelocidad: {self.__velocidad}\nPeso: {self.__peso}"
+    def calcularDespacho(self):
+        valorKilo = 300
+        totalDespacho =  super().calcularDespacho(self.__peso, valorKilo)
+        return totalDespacho
         
+    def calcularDescuento(self):
+        return super().calcularDescuento()
     
+    def __str__(self):
+        precioDescuento, totalPagarDescuento = self.calcularDescuento()
+        costoDespacho = self.calcularDespacho()
+        TotalDespachoDescuento = totalPagarDescuento + costoDespacho
+        return f"{super().__str__()}\nAro: {self.__aro}\nVelocidad: {self.__velocidad}\nPeso: {self.__peso}\nDescuento: {precioDescuento}\nCosto del despacho: {costoDespacho}\nValor Total: {TotalDespachoDescuento}"
+        
+
 PRODUCTO1 = Scooter(30, 90.8, "C", "Sushipleto", 20.1, 100, 100.4, 10000)
 print(PRODUCTO1)
